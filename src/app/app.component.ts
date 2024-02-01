438054e480edd359a8af21ea2780929bde40b873
renatu <renatu_mb@yahoo.com.br>
2024-02-01 00:52:53 -0800

    main page - sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {NgClass} from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    NgClass
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tailwind22';
+  public menuOpen: boolean = false;
 }
